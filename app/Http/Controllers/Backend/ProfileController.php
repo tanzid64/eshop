27c3d7ad6155401d501323cb4ad2bfd98fc31963
Tanzid Haque <tanzid3@gmail.com>
2025-08-23 00:26:35 +0600

    Password change admin

--- app/Http/Controllers/Backend/ProfileController.php
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 use function Illuminate\Log\log;
@@ -18,29 +20,53 @@ class ProfileController extends Controller
 
     public function updateProfile(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users,email,' . auth()->user()->id,
-            'phone' => 'nullable|string|max:255',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
-        $user = auth()->user();
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|max:255|unique:users,email,' . auth()->user()->id,
+                'phone' => 'nullable|string|max:255',
+                'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+            $user = auth()->user();
 
-        if ($request->hasFile('image')) {
-            $user->deleteUserImage();
-            $image = $request->file('image');
-            $userId = $user->id;
-            $timestamp = time();
-            $extension = $image->getClientOriginalExtension();
-            $imageName = "avatar_{$userId}_{$timestamp}.{$extension}";
-            $path = Storage::disk('supabase')->putFileAs('profiles', $image, $imageName, 'public');
-            $user->image = $path;
+            if ($request->hasFile('image')) {
+                $user->deleteUserImage();
+                $image = $request->file('image');
+                $userId = $user->id;
+                $timestamp = time();
+                $extension = $image->getClientOriginalExtension();
+                $imageName = "avatar_{$userId}_{$timestamp}.{$extension}";
+                $path = Storage::disk('supabase')->putFileAs('profiles', $image, $imageName, 'public');
+                $user->image = $path;
+            }
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->save();
+
+            return redirect()->route('admin.profile')->with('success', 'Profile updated successfully');
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return redirect()->route('admin.profile')->with('error', 'Profile update failed');
         }
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->phone = $request->phone;
-        $user->save();
+    }
 
-        return redirect()->route('admin.profile')->with('success', 'Profile updated successfully');
+    public function updatePassword(Request $request)
+    {
+        try {
+            $request->validate([
+                'current_password' => 'required|current_password|max:255',
+                'password' => 'required|string|max:255|confirmed',
+            ]);
+
+            $user = auth()->user();
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            return redirect()->route('admin.profile')->with('success', 'Password updated successfully');
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return redirect()->route('admin.profile')->with('error', 'Password update failed');
+        }
     }
 }
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -32,26 +32,117 @@
                                     </div>
                                     <div class="form-group col-12">
                                         <label for="image">Image</label>
-                                        <input id="image" type="file" name="image" class="form-control">
+                                        <input id="image" type="file" name="image"
+                                            class="form-control @error('image') is-invalid @enderror">
+                                        @error('image')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
                                     <div class="form-group col-md-6 col-12">
                                         <label for="name">Full Name</label>
-                                        <input type="text" name="name" class="form-control"
+                                        <input type="text" name="name"
+                                            class="form-control @error('name') is-invalid @enderror"
                                             value="{{ auth()->user()->name }}" required="">
+                                        @error('name')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
                                     <div class="form-group col-md-6 col-12">
                                         <label for="email">Email</label>
-                                        <input type="email" name="email" class="form-control"
+                                        <input type="email" name="email"
+                                            class="form-control @error('email') is-invalid @enderror"
                                             value="{{ auth()->user()->email }}" required="">
-
+                                        @error('email')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="row">
-
                                     <div class="form-group col-md-6 col-12">
                                         <label>Phone</label>
                                         <input type="tel" name="phone" class="form-control"
                                             value="{{ auth()->user()->phone }}">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer text-right">
+                                <button class="btn btn-primary">Save Changes</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-12 col-md-12 col-lg-5">
+                    <div class="card">
+                        <form method="post" action="{{ route('admin.profile.update-password') }}" class="needs-validation"
+                            novalidate="">
+                            @csrf
+                            <div class="card-header">
+                                <h4>Change Password</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <div class="form-group col-12">
+                                        <label for="current_password">Current Password</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <i class="fas fa-lock"></i>
+                                                </div>
+                                            </div>
+                                            <input id="current_password" type="password" name="current_password"
+                                                class="form-control @error('current_password') is-invalid @enderror">
+                                            @error('current_password')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group col-12">
+                                        <label for="password">New Password</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <i class="fas fa-lock"></i>
+                                                </div>
+                                            </div>
+                                            <input id="password" type="password" name="password"
+                                                class="form-control @error('password') is-invalid @enderror">
+                                            @error('password')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group col-12">
+                                        <label for="password_confirmation">Confirm New Password</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <i class="fas fa-lock"></i>
+                                                </div>
+                                            </div>
+                                            <input id="password_confirmation" type="password" name="password_confirmation"
+                                                class="form-control @error('password_confirmation') is-invalid @enderror">
+                                            @error('password_confirmation')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
