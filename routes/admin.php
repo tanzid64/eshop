a539d69a63062f4aa40b0cc5303aa3a2ee9a05c3
Tanzid Haque <tanzid3@gmail.com>
2025-08-25 21:12:06 +0600

    All slider ui

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 // Admin Routes
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
@@ -10,3 +11,6 @@ Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard'
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update-password', [ProfileController::class, 'updatePassword'])->name('profile.update-password');
+
+// Slider Routes
+Route::resource('slider', SliderController::class);
